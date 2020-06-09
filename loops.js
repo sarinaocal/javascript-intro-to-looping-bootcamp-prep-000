7badfb67caf71dc225a2e9e6b95b022363eceb3c
Sarina Ocal <sarinaocal@gmail.com>
2020-06-09 01:20:26 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,2 +1,4 @@
 function doWhileLoop(`num += 1`)
-  console.log incrementVariable("I run once regardless.")
+  console.log incrementVariable("I run once regardless.")for ([initialization]; [condition]; [iteration]) {
+  [loopBody];
+}
